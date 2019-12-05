2dde0984973bd5d54f4c3899f6e1b1aae2df4f50
Clemens <clemens@machelp.nl>
2019-12-05 15:22:32 +0100

    Working slideshow for rounds

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,4 +1,85 @@
 @extends('layouts.app')
+<style>
+    * {box-sizing: border-box}
+    body {font-family: Verdana, sans-serif; margin:0}
+    .mySlides {display: none}
+    img {vertical-align: middle;}
+
+    /* Slideshow container */
+    .slideshow-container {
+        max-width: 1000px;
+        position: relative;
+        margin: auto;
+    }
+
+    /* Next & previous buttons */
+    .prev, .next {
+        cursor: pointer;
+        position: absolute;
+        top: 31%;
+        width: auto;
+        padding: 16px;
+        margin-top: -22px;
+        color: white;
+        font-weight: bold;
+        font-size: 18px;
+        transition: 0.6s ease;
+        border-radius: 0 3px 3px 0;
+        user-select: none;
+    }
+
+    /* Position the "next button" to the right */
+    .next {
+        right: 0;
+        border-radius: 3px 0 0 3px;
+    }
+
+    /* On hover, add a black background color with a little bit see-through */
+    .prev:hover, .next:hover {
+        background-color: rgba(0,0,0,0.8);
+    }
+
+    /* Caption text */
+    .text {
+        color: #f2f2f2;
+        font-size: 15px;
+        padding: 8px 12px;
+        position: absolute;
+        bottom: 8px;
+        width: 100%;
+        text-align: center;
+    }
+
+    /* Number text (1/3 etc) */
+    .numbertext {
+        color: #f2f2f2;
+        font-size: 12px;
+        padding: 8px 12px;
+        position: absolute;
+        top: 0;
+    }
+
+    /* The dots/bullets/indicators */
+    .dot {
+        cursor: pointer;
+        height: 15px;
+        width: 15px;
+        margin: 0 2px;
+        background-color: #bbb;
+        border-radius: 50%;
+        display: inline-block;
+        transition: background-color 0.6s ease;
+    }
+
+    .active, .dot:hover {
+        background-color: #717171;
+    }
+
+    /* On smaller screens, decrease text size */
+    @media only screen and (max-width: 300px) {
+        .prev, .next,.text {font-size: 11px}
+    }
+</style>
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
@@ -99,24 +180,81 @@
                 </div>
 
                 <div id="Rounds" class="w3-container w3-border tab" style="display:none">
+{{--                    <div class="slideshow-container">--}}
+
+{{--                        <div class="mySlides fade">--}}
+{{--                            <div class="numbertext">1 / 3</div>--}}
+{{--                            <img src="https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg" style="width:100%">--}}
+{{--                            <div class="text">Caption Text</div>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="mySlides fade">--}}
+{{--                            <div class="numbertext">2 / 3</div>--}}
+{{--                            <img src="https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg" style="width:100%">--}}
+{{--                            <div class="text">Caption Two</div>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="mySlides fade">--}}
+{{--                            <div class="numbertext">3 / 3</div>--}}
+{{--                            <img src="https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg" style="width:100%">--}}
+{{--                            <div class="text">Caption Three</div>--}}
+{{--                        </div>--}}
+
+{{--                        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>--}}
+{{--                        <a class="next" onclick="plusSlides(1)">&#10095;</a>--}}
+
+{{--                    </div>--}}
+{{--                    <br>--}}
+
                     @php
                         $league =
                 [
-                    ["Team 1", "Team 2", "-", "Team 3", "Team 4", "-", "Team 5", "Team 6", "-", "Team 7", "Team 8"],
-                    ["Team 1", "Team 2", "-", "Team 3", "Team 4", "-", "Team 5", "Team 6", "-", "Team 7", "Team 8"],
-                    ["Team 1", "Team 2", "-", "Team 3", "Team 4", "-", "Team 5", "Team 6", "-", "Team 7", "Team 8"],
-                    ["Team 1", "Team 2", "-", "Team 3", "Team 4", "-", "Team 5", "Team 6", "-", "Team 7", "Team 8"],
+                    [" Team 1", " Team 2 <hr>", " Team 3", " Team 4 <hr>", " Team 5", " Team 6 <hr>", " Team 7", " Team 8 <hr>", " Team 9", " Team 10 <hr>", " Team 11", " Team 12 <hr>"],
+                    [" Team 1 -", " Team 2 <hr>", " Team 3", " Team 4 <hr>", " Team 5", " Team 6 <hr>", " Team 7", " Team 8 <hr>", " Team 9", " Team 10 <hr>", " Team 11", " Team 12 <hr>"],
+                    [" Team 1 +", " Team 2 <hr>", " Team 3", " Team 4 <hr>", " Team 5", " Team 6 <hr>", " Team 7", " Team 8 <hr>", " Team 9", " Team 10 <hr>", " Team 11", " Team 12 <hr>"],
+                    [" Team 1 ?", " Team 2 <hr>", " Team 3", " Team 4 <hr>", " Team 5", " Team 6 <hr>", " Team 7", " Team 8 <hr>", " Team 9", " Team 10 <hr>", " Team 11", " Team 12 <hr>"],
                 ];
 
                         for ($row = 0; $row < count($league); $row++) {
-                            echo "<p><b>Row number $row</b></p>";
+                                echo "<div class='mySlides'>";
+                            echo '<a class="prev" onclick="plusSlides(-1)">&#10094;</a>';
+                            echo '<a class="next" onclick="plusSlides(1)">&#10095;</a>';
+                            echo "<h2 style='text-align: center; margin-top: 20px; padding-bottom: 25px'><b>Ronde $row</b></h2>";
                             for ($col = 0; $col < count($league[1]); $col++) {
                                 // Now every team has it's own callable name, which is the same value as that what is being shown.
-                                echo "<span><input type='radio' name='" . $league[$row][$col] . "' value='other'>" . $league[$row][$col] . "</span><br>";
+                                    echo "<span><input type='radio' name='" . $league[$row][$col] . "' value='other'>" . $league[$row][$col] . "</span><br>";
                             }
+                                echo "</div>";
                         }
                     @endphp
+                    <script>
+                        var slideIndex = 1;
+                        showSlides(slideIndex);
 
+                        function plusSlides(n) {
+                            showSlides(slideIndex += n);
+                        }
+
+                        function currentSlide(n) {
+                            showSlides(slideIndex = n);
+                        }
+
+                        function showSlides(n) {
+                            var i;
+                            var slides = document.getElementsByClassName("mySlides");
+                            var dots = document.getElementsByClassName("dot");
+                            if (n > slides.length) {slideIndex = 1}
+                            if (n < 1) {slideIndex = slides.length}
+                            for (i = 0; i < slides.length; i++) {
+                                slides[i].style.display = "none";
+                            }
+                            for (i = 0; i < dots.length; i++) {
+                                dots[i].className = dots[i].className.replace(" active", "");
+                            }
+                            slides[slideIndex-1].style.display = "block";
+                            dots[slideIndex-1].className += " active";
+                        }
+                    </script>
 
 {{--                    @for($row = 0; $row < count($league); $row++)--}}
 {{--                        {{var_dump($league)}}--}}
